7dcf0062a4f25d1e13e0ac8feff89c524adee888
PabluskiNC <PabluskiNC@users.noreply.github.com>
2016-05-14 00:18:06 -0400

    Update coopController.ino

--- Arduino/coopController/coopController.ino
+++ Arduino/coopController/coopController.ino
@@ -17,17 +17,18 @@
 #include "secrets.h"              // holds wifi credentials & MQTT credentials
 
 // print debug messages or not to serial
-int Debugging = 0;
+int Debugging = 1;
 
 // Digital & analog pins for various components
 
 const int lightSense    = A0; // Light Sensor
-const int doorOpen      = D6; // MotorA CCW UP
-const int doorClose     = D5; // MotorA CW DOWN
+
 const int doorTop       = D1; // Reed Switch
 const int doorBottom    = D2; // Reed Switch
 const int rtcSDA        = D3; // Real Time Clock SDA (i2c ESP8266 NodeMCU 0.9)
 const int rtcSCL        = D4; // Real Time Clock SCL (i2c ESP8266 NodeMCU 0.9)
+const int doorClose     = D5; // MotorA CW DOWN
+const int doorOpen      = D6; // MotorA CCW UP
 
 // WIFI Settings ** Moved to secrets.h **
 //#define wHost "wifi"
@@ -200,7 +201,7 @@ void mqttData(char* topic, byte* payload, unsigned int plen) {
       }
     }
     if (data == "debug") {
-      Debugging=1;
+      Debugging = 1 - Debugging;
     }
     if (data == "door") {
       if(Debugging) {
